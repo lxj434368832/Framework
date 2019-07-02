33d1e93c5dbe405c443231e64764649c9c421cc3
明巧文 <434368832@qq.com>
2019-07-02 11:46:25 +0800

    1、去掉无用的宏定义。

--- include/GlobalDefine.h
+++ include/GlobalDefine.h
@@ -9,10 +9,4 @@
 #define logd() ::SetConsoleTextAttribute(consolehwnd, 7);std::cout<<"[D]:"
 #define logt() ::SetConsoleTextAttribute(consolehwnd, 4);std::cout<<"[T]:"<<::GetTickCount64()
 //#define logt() ::system("color 4");std::cout<<"[T]:"
-#else
-#define logm() LogInfo()<<"["<<__FUNCTION__<<"]|<message> "
-#define logw() LogInfo()<<"["<<__FUNCTION__<<"]|<warn> "
-#define loge() LogInfo()<<"["<<__FUNCTION__<<"]|<error> "
-#define logd() LogInfo()<<"["<<__FUNCTION__<<"]|<debug> "
-#define logt() LogInfo()<<"["<<__FUNCTION__<<"]|<trace> "
 #endif
