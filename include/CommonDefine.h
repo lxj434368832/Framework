3e1960ca792f72ed9b85e99eb67a2c21a7ecedba
明巧文 <434368832@qq.com>
2020-04-13 11:23:31 +0800

    1.添加局部变量的宏定义。

--- include/CommonDefine.h
+++ include/CommonDefine.h
@@ -17,12 +17,22 @@ inline Class* q_func() { return static_cast<Class *>(q_ptr); } \
 inline const Class* q_func() const { return static_cast<const Class *>(q_ptr); } \
 friend class Class;
 
+#define DEFINE_PRIVATE(Class) \
+	Class##Private* const d_ptr;
+
+#define DEFINE_PUBLIC(Class) \
+	Class* const q_ptr;
+
+
 #define Q_D(Class) Class##Private * const d = d_func()
 #define Q_Q(Class) Class * const q = q_func()
 
+
+#define DEFINE_DATA(Class) \
+	Class##Data* const d;
 
 #define DECLARE_DATA(Class) \
 	inline Class##Data* d_func() { return reinterpret_cast<Class##Data *>(d); } \
 	inline const Class##Data* d_func() const { return reinterpret_cast<const Class##Data *>(d); }
 
-#define M_D(Class) Class##Data* const d = d_func()
+//#define M_D(Class) Class##Data* const d = d_func()
